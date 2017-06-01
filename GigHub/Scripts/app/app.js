7653f982746488ce75822a24013fbebe9bc5cd37
alex.paclin <alex.paclin@gmail.com>
2017-06-01 18:24:02 +0500

    20160524204525_2

---
+++ GigHub/Scripts/app/app.js
@@ -0,0 +1,38 @@
+﻿var GigsController = function () {
+	var button;
+
+	var init = function () {
+		$(".js-toggle-attendance").click(toggleAttendance);
+	};
+	
+	function toggleAttendance(e) {
+		button = $(e.target);
+		if (button.hasClass("btn-default")) {
+			$.post("/api/attendances", { gigId: button.attr("data-gig-id") })
+				.done(done)
+				.fail(fail);
+		} else {
+			$.ajax({
+				url: "/api/attendances/" + button.attr("data-gig-id"),
+				method: "DELETE"
+			})
+				.done(done)
+				.fail(fail);
+		}
+	};
+
+	function done() {
+		var text = (button.text() === "Going") ? "Going?" : "Going";
+		button.toggleClass("btn-info")
+			.toggleClass("btn-default")
+			.text(text);
+	};
+
+	function fail() {
+		alert("Something failed!");
+	};
+
+	return {
+		init: init
+	};
+}();
