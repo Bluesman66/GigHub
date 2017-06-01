d8a7a74867223075b7ea1c292dbd7383b46b5300
alex.paclin <alex.paclin@gmail.com>
2017-06-01 22:39:02 +0500

    20160524204527_2

---
+++ GigHub/Scripts/app/services/attendanceService.js
@@ -0,0 +1,17 @@
+﻿var AttendanceService = function () {
+	function createAttendance(gigId, done, fail) {
+		$.post("/api/attendances", { gigId: gigId }).done(done).fail(fail);
+	};
+
+	function deleteAttendance(gigId, done, fail) {
+		$.ajax({
+			url: "/api/attendances/" + gigId,
+			method: "DELETE"
+		}).done(done).fail(fail);
+	}
+
+	return {
+		createAttendance: createAttendance,
+		deleteAttendance: deleteAttendance
+	}
+}();
