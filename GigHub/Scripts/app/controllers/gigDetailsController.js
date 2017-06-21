4b3363f99fe02b6ed54c58a17061aef48a6f3b1c
alex.paclin <alex.paclin@gmail.com>
2017-06-21 17:52:15 +0500

    Modularizing JavaScript Code

---
+++ GigHub/Scripts/app/controllers/gigDetailsController.js
@@ -0,0 +1,31 @@
+﻿var GigDetailsController = function (followingService) {
+	var followButton;
+
+	function init() {
+		$(".js-toggle-follow").click(toggleFollowing);
+	};
+	
+	function toggleFollowing(e) {
+		followButton = $(e.target);
+		var followeeId = followButton.attr("data-user-id");
+		if (followButton.hasClass("btn-default"))
+			followingService.createFollowing(followeeId, done, fail);
+		else
+			followingService.deleteFollowing(followeeId, done, fail);
+	};
+
+	function done() {
+		var text = (followButton.text().trim() === "Follow") ? "Following" : "Follow";
+		followButton.toggleClass("btn-info")
+			.toggleClass("btn-default")
+			.text(text);
+	};
+
+	function fail() {
+		alert("Something failed!");
+	};
+
+	return {
+		init: init
+	};
+}(FollowingService);
---
+++ GigHub/Scripts/app/services/followingService.js
@@ -0,0 +1,17 @@
+﻿var FollowingService = function() {
+	function createFollowing(followeeId, done, fail) {
+		$.post("/api/followings", { followeeId: followeeId }).done(done).fail(fail);
+	};
+
+	function deleteFollowing(followeeId, done, fail) {
+		$.ajax({
+			url: "/api/followings/" + followeeId,
+			method: "DELETE"
+		}).done(done).fail(fail);
+	}
+
+	return {
+		createFollowing: createFollowing,
+		deleteFollowing: deleteFollowing
+	}
+}();
