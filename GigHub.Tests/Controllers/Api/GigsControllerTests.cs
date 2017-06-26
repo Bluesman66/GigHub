e328c82aff532445524c64b421f8bddd5297f9fb
alex.paclin <alex.paclin@gmail.com>
2017-06-26 23:56:02 +0500

    20160524204546_2

---
+++ GigHub.Tests/Controllers/Api/GigsControllerTests.cs
@@ -0,0 +1,51 @@
+﻿using FluentAssertions;
+using GigHub.Controllers.Api;
+using GigHub.Core;
+using GigHub.Core.Models;
+using GigHub.Core.Repositories;
+using GigHub.Tests.Extensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Web.Http.Results;
+
+namespace GigHub.Tests.Controllers.Api
+{
+	[TestClass]
+	public class GigsControllerTests
+	{
+		private GigsController _gigsController;
+		private Mock<IGigRepository> _mockRepository;
+
+		public GigsControllerTests()
+		{
+			_mockRepository = new Mock<IGigRepository>();
+
+			var mockUoW = new Mock<IUnitOfWork>();
+			mockUoW.SetupGet(u => u.Gigs).Returns(_mockRepository.Object);
+
+			_gigsController = new GigsController(mockUoW.Object);
+			_gigsController.MockCurrentUser("1", "user@domain.com");
+		}
+
+		[TestMethod]
+		public void Cancel_NoGigWithGivenIdExists_ShouldReturnNotFound()
+		{
+			var result = _gigsController.Cancel(1);
+
+			result.Should().BeOfType<NotFoundResult>();
+		}
+
+		[TestMethod]
+		public void Cancel_GigIsCancelled_ShouldReturnNotFound()
+		{
+			var gig = new Gig();
+			gig.Cancel();
+
+			_mockRepository.Setup(r => r.GetGigWithAttendees(1)).Returns(gig);
+
+			var result = _gigsController.Cancel(1);
+
+			result.Should().BeOfType<NotFoundResult>();
+		}
+	}
+}
